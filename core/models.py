4cf24e5a290365f73094bb98966c972cb8913cbb
Kiran Kumari <kiran1999rp@gmail.com>
2022-02-10 11:21:38 +0530

    add custom user model

---
+++ core/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, *args, **kwargs):
+        if not phone_number:
+            raise ValueError("Users must have phone number")
+        user = self.model(phone_number=phone_number, *args, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password):
+        user = self.create_user(phone_number, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=512, unique=True, blank=True, null=True)
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=10, unique=True, blank=False, null=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'phone_number'

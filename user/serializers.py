cdad2ded65d12741e70db5b66caf83731149d522
Kiran Kumari <kiran1999rp@gmail.com>
2022-02-10 11:42:17 +0530

    create view and urls

---
+++ user/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["phone_number", "password", "name"]
+        extra_kwargs = {
+            "password": {"write_only": True, "min_length": 5},
+            "phone_number": {"min_length": 10, "max_length": 10},
+        }
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
